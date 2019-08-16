afded1981a6e5bb74c1333c0d3d3b60137153f57
Jekabs Karklins <jekabs.karklins@esss.se>
2019-08-16 15:49:36 +0200

    Minor tweaks

--- src/ExampleForm.js
+++ src/ExampleForm.js
@@ -1,7 +1,6 @@
 
 
 import React from 'react';
-import { render } from 'react-dom';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 
@@ -26,12 +25,10 @@ export default function ExampleForm(props) {
           values,
           touched,
           errors,
-          dirty,
           isSubmitting,
           handleChange,
           handleBlur,
           handleSubmit,
-          handleReset,
           validateField
         } = props;
         return (
@@ -54,22 +51,9 @@ export default function ExampleForm(props) {
               <div className="input-feedback">{errors.email}</div>
             )}
 
-            <button
-              type="button"
-              className="outline"
-              onClick={handleReset}
-              disabled={!dirty || isSubmitting}
-            >
-              Reset
-            </button>
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-
-            <button type="button" onClick={ () => {validateField("email")} }>
-              Next step
-            </button>
-
+            {/* <button type="submit" disabled={isSubmitting}> Submit </button> */}
+            <button type="button" onClick={ () => {validateField("email")} }> Next step </button>
+            
           </form>
         );
       }}
